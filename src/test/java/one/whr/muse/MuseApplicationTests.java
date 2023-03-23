472e6db2c45049873a5066d0f81ea5437e5f116e
HAoRAn-W <haoran2326@gmail.com>
2023-03-23 19:10:11 -0400

    update origins

--- src/test/java/one/whr/muse/MuseApplicationTests.java
+++ src/test/java/one/whr/muse/MuseApplicationTests.java
@@ -1,6 +1,5 @@
 package one.whr.muse;
 
-import one.whr.muse.entity.met.MetArtwork;
 import one.whr.muse.repository.MetArtworkRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
