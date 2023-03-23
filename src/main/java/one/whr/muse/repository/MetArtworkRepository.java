2a1fc9345a10741062f9c5f25008aa49d0c3eb49
HAoRAn-W <haoran2326@gmail.com>
2023-03-23 19:10:43 -0400

    Revert "update origins"
    This reverts commit 472e6db2c45049873a5066d0f81ea5437e5f116e.

--- src/main/java/one/whr/muse/repository/MetArtworkRepository.java
+++ src/main/java/one/whr/muse/repository/MetArtworkRepository.java
@@ -2,11 +2,9 @@ package one.whr.muse.repository;
 
 import one.whr.muse.entity.met.MetArtwork;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MetArtworkRepository extends MongoRepository<MetArtwork, Float> {
     List<MetArtwork> findAll();
 }
